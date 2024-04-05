8a62b1d7e80f2e2cdc0179a78cefc87ecbb7adec
luf-r <166117515+luf-r@users.noreply.github.com>
2024-04-05 02:44:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const token = process.env.token;
+
+if (!token) {
+  console.error("Missing Discord bot token.");
+  process.exit(1);
+}
+
+const bot = new Eris(token);
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error("An error occurred:", err); // or your preferred logger
+});
+
+bot.on("ready", () => {
+  console.log("Bot is ready!");
+  bot.editStatus("dnd", {
+    name: "احبكم كلكم",
+    type: 3, // Activity type: playing
+    url: "https://discordapp.com", // Optional: a URL to link to
+  });
 });
 
 bot.connect(); // Get the bot to connect to Discord
